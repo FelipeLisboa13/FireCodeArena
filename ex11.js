84be9afe0cd236ee2bff51b84993244d401a2287
FelipeLisboa <lisboafelipe38@gmail.com>
2026-06-16 17:01:09 -0300

    Subindo para o git

---
+++ ex11.js
@@ -0,0 +1,12 @@
+let n = [5, 8, 3, 9, 2]
+let d = []
+console.log("")
+console.log("-------------- Dobro dos números -----------------")
+console.log("")
+for (let i = 0; i < n.length; i++) {
+    d[i] = n[i] * 2 
+}
+console.log("Números identificados: " + n)
+console.log("")
+console.log("Números dobrados: " + d)   
+console.log("")
